8a395cba0ea45eab9c4fb97795de75861cb06694
Iamneo <techadmin@iamneo.ai>
2023-09-07 05:48:12 +0000

    Backup Commit

--- Cucumber/src/test/java/stepdefination/run.java
+++ Cucumber/src/test/java/stepdefination/run.java
@@ -5,11 +5,5 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-    features = "src/test/resources/features",
-    glue = "stepdefination",
-    plugin = {"pretty", "html:target/cucumber-reports"}
-)
-public class run {
-    // This class will not have any code. It simply acts as a runner for Cucumber tests.
-}
+@Cucumber.Options(format = {"pretty", "html:target/cucumber"})
+public class run { }
