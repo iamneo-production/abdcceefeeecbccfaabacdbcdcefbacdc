bd1239bf0f923b866c2e89ae8034c606f5c81e5e
Iamneo <techadmin@iamneo.ai>
2023-09-07 05:43:12 +0000

    Backup Commit

--- Cucumber/src/test/java/stepdefination/run.java
+++ Cucumber/src/test/java/stepdefination/run.java
@@ -1,13 +1,15 @@
+package stepdefination;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/features", // Path to your feature files
-    glue = "stepdefination", // Package where your step definitions are located
+    features = "src/test/resources/features",
+    glue = "stepdefination",
     plugin = {"pretty", "html:target/cucumber-reports"}
 )
-public class Runner {
+public class run {
     // This class will not have any code. It simply acts as a runner for Cucumber tests.
 }
